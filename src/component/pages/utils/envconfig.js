68b245bd24024ad68fc2597eb11603b84c0ec13b
Sabbir Ahmmed <srgamingzone71@gmail.com>
2026-06-26 19:41:02 +0600

    api key update

--- src/component/pages/utils/envconfig.js
+++ src/component/pages/utils/envconfig.js
@@ -1,6 +1,6 @@
 const config = {
     backendUrl: import.meta.env.VITE_BACKEND_BASE_URL,
-    VIT_SURVEY_API_KEY: import.meta.env.VIT_SURVEY_API_KEY
+    VITE_SURVEY_API_KEY: import.meta.env.VITE_SURVEY_API_KEY
 };
 
 export default config;
